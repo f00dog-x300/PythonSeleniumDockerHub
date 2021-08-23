84ebaa7ac08e332352550ab047f993535feedfd3
Kevin <f00dog.x300@gmail.com>
2021-08-22 22:02:39 -0500

    moved notes to README

---
+++ README.md
@@ -0,0 +1,14 @@
+## Notes: 
+
+- Original source obtained from : https://testdriven.io/blog/distributed-testing-with-selenium-grid/
+
+- Versions of Selenium/Hub images; look under tag tab
+    - https://hub.docker.com/r/selenium/hub/tags?page=1&ordering=last_updated
+
+- To run: 
+    export BROWSER=chrome && python Tests/test_example.py
+
+- Attempt to use noVNC
+    - https://github.com/LibVNC/x11vnc
+    - Installation : Windows Installation 
+        - npm install @novnc/novnc
--- Tests/test_example.py
+++ Tests/test_example.py
@@ -53,18 +53,3 @@ class HackernewsSearchTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-
-"""
-Notes: 
-- Original source obtained from : https://testdriven.io/blog/distributed-testing-with-selenium-grid/
-- Versions of Selenium/Hub images; look under tag tab
-    - https://hub.docker.com/r/selenium/hub/tags?page=1&ordering=last_updated
-- To run: 
-    export BROWSER=chrome && python Tests/test_example.py
-
-- Attempt to use noVNC
-    - https://github.com/LibVNC/x11vnc
-    - Installation : Windows Installation 
-        - npm install @novnc/novnc
-"""
